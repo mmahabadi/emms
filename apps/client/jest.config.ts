f42eac6aad66d58caee08c8aac1042fb835bbd7b
mmahabadi <m.mahabadi@sadad.co.ir>
2022-06-03 13:11:49 +0430

    rename client projects

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'emms',
+  displayName: 'client',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/emms',
+  coverageDirectory: '../../coverage/apps/client',
 };
