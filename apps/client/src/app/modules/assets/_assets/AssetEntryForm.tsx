eb092d35b660fc5ae777746ea20799a5e8cb3b3e
mmahabadi <m.mahabadi@sadad.co.ir>
2022-07-28 16:56:30 +0430

    add uuid in asset-entry-form

--- apps/client/src/app/modules/assets/_assets/AssetEntryForm.tsx
+++ apps/client/src/app/modules/assets/_assets/AssetEntryForm.tsx
@@ -6,6 +6,7 @@ import {yupResolver} from "@hookform/resolvers/yup";
 import {Datepicker, ModalFormContainer, setFormValues, TextInput, useModalConfig} from "@emms/ui-kit";
 import {SelectAsset, SelectAssetCat, SelectLocation, SelectOrg} from "../../../helpers";
 import {saveAsset} from "../core/services";
+import {v4 as uuidv4} from 'uuid';
 
 const formSchema = yup.object().shape({
   assetCatId: yup.object().required(),
@@ -35,6 +36,7 @@ export const AssetEntryForm: FC = () => {
     };
     delete values.invalid_from;
     delete values.plate_no;
+    if(!selectedItem) values.id = uuidv4();
     setFormValues(form, values);
   }
 
